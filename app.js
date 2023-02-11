a1c5c2e186f63e724aab59024a53651257d65fbe
aria198a <aria198a@gmail.com>
2023-02-11 13:06:28 +0800

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 const app = express()
 const restaurantList = require('./restaurant.json')
 const port = 3000
 
 //setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
 
 //setting static files
